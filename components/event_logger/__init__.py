df5e90dd35f5e386e0cb2c19bd1cc68e19e031f5
atiburzi <96880575+atiburzi@users.noreply.github.com>
2025-07-28 23:05:11 +0200

    Update __init__.py

--- components/event_logger/__init__.py
+++ components/event_logger/__init__.py
@@ -2,23 +2,28 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-# Define the C++ namespace and class for our component
-# This should match the namespace and class name in event_logger.h
+# Aggiungiamo questo import
+import esphome.components.time as time 
+# Non servono le action import ancora, se vogliamo isolare
+
 event_logger_ns = cg.esphome_ns.namespace("event_logger")
 EventLogger = event_logger_ns.class_("EventLogger", cg.Component)
 
-# Configuration schema for the 'event_logger:' entry in YAML
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(EventLogger),
         cv.Optional("max_entries", default=10): cv.All(cv.int_, cv.Range(min=1)),
+        # Aggiungiamo time_id allo schema del componente logger
+        cv.Required("time_id"): cv.use_id(time.RealTimeClock), 
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
-# To_code function: This tells ESPHome how to generate C++ code from the YAML config
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
     if "max_entries" in config:
         cg.add(var.set_max_entries(config["max_entries"]))
+    
+    # NON AGGIUNGERE ANCORA la logica per set_time_component qui
+    # NON AGGIUNGERE ANCORA la registrazione del servizio qui
